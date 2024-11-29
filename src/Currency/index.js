34923d486679b4db1b566a650f3dae2392c4d2a5
Adrian Markefka <am97veb@gmail.com>
2024-11-29 15:54:19 +0100

    creating the Currency component

---
+++ src/Currency/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Currency = () => (
+    <select className="form__field" >
+        <option>EUR</option>
+        <option>USD</option>
+        <option>CHF</option>
+    </select>
+);
+
+export default Currency;
