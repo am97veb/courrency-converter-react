ea89a977d022c9ea6c8650aee9203d2fc03f696a
Adrian Markefka <am97veb@gmail.com>
2024-11-29 15:54:42 +0100

    creating the Result component

---
+++ src/Result/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Result = () => (
+    <p>
+        Wynik:
+        <span className="result">
+            brak
+        </span>
+    </p>
+
+);
+
+export default Result;
