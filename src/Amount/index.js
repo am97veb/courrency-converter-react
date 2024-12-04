e0ddd1ef0f095b58c43761ea3f37bd2599509626
Adrian Markefka <am97veb@gmail.com>
2024-12-04 16:53:26 +0100

    value and onChange attribute add

--- src/Amount/index.js
+++ src/Amount/index.js
@@ -1,14 +1,16 @@
 import "./style.css";
 
-const Amount = () => (
+const Amount = ({amountValue, setAmount}) => (
     <input
+        value={amountValue}
+        onChange={(event) => setAmount(event.target.value)}
         className="form__field"
-        name="amound"
+        name="amount"
         type="number"
         min="1"
         step="any"
         required
     />
-);
+)
 
 export default Amount;
