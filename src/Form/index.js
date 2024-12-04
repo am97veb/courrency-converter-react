bf5b380af160f93f8962553ea2e9070ab5c0d60e
Adrian Markefka <am97veb@gmail.com>
2024-12-04 16:58:34 +0100

    onFormSubmit add

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,9 +1,19 @@
 import "./style.css";
 
-const Form = ({children}) => (
-    <form className="form">
-        {children}
-    </form>
-);
+const Form = ({ children }) => {
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        < form
+            onSubmit={onFormSubmit}
+            className="form"
+        >
+            {children}
+        </form >
+    );
+};
 
 export default Form;
