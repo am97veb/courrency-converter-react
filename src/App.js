588dbc828280b8afc7e6d4017457172dd66837d4
Adrian Markefka <am97veb@gmail.com>
2024-12-04 16:57:34 +0100

    add: useState, currencyTable, filterNameOnTable, currencyValue and result function

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ import Currency from "./Currency";
 import Result from "./Result";
 import Button from "./Button";
 import Fieldset from "./Fieldset";
+import { useState } from "react";
 
 function App() {
+  const [currencyName, setCurrency] = useState("EUR");
+  const [amountValue, setAmount] = useState("");
+  const [resultValue, setResultValue] = useState("brak");
+
+  const currencyTable = [
+    { id: 1, name: "EUR", value: 4.34 },
+    { id: 2, name: "USD", value: 4.03 },
+    { id: 3, name: "CHF", value: 4.57 },
+  ];
+
+  const filterNameOnTable = currencyTable.filter(({ name }) => name === currencyName);
+  const currencyValue = filterNameOnTable.map(({ value }) => value);
+
+  const result = () => {
+    return amountValue / currencyValue
+  };
+
   return (
 
     <Form>
@@ -15,14 +33,35 @@ function App() {
       >
         <Label
           labelText="Waluta:"
-          formField={<Currency />}
+          formField={
+            <Currency
+              currencyTable={currencyTable}
+              currencyName={currencyName}
+              setCurrency={setCurrency}
+            />
+          }
         />
         <Label
           labelText="Kwota do przeliczenia:"
-          formField={<Amount />}
+          formField={
+            <Amount
+              amountValue={amountValue}
+              setAmount={setAmount}
+            />
+          }
         />
-        <Result />
-        <Button />
+        <Result
+          resultValue={resultValue}
+          amountValue={amountValue}
+          currencyName={currencyName}
+        />
+        <Button
+          result={result}
+          resultValue={resultValue}
+          setResultValue={setResultValue}
+          amountValue={amountValue}
+          currencyName={currencyName}
+        />
       </Fieldset>
     </Form>
   )
