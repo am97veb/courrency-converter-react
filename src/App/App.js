a06501e0eb80fff55b480d057317aab02a99de82
Adrian Markefka <am97veb@gmail.com>
2024-12-23 15:53:14 +0100

    create App folder and moving there App.js file and folders: clock, form and result

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,13 @@
 import Form from "./Form";
-import Result from "./Result";
+import Result from "./Form/Result";
 import Clock from "./Clock";
+import { currencyTable } from "./currencyTable";
 import { useState } from "react";
 
 function App() {
   const [currencyName, setCurrency] = useState("EUR");
   const [amountValue, setAmount] = useState("");
   const [result, setResult] = useState(null);
-
-  const currencyTable = [
-    { id: 1, name: "EUR", value: 4.34 },
-    { id: 2, name: "USD", value: 4.03 },
-    { id: 3, name: "CHF", value: 4.57 },
-  ];
 
   const filterNameOnTable = currencyTable.filter(({ name }) => name === currencyName);
   const currencyValue = filterNameOnTable.map(({ value }) => value);
