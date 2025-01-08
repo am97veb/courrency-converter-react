0456ac5415660c93c6c1627b7b0a8d77fa14350d
Adrian Markefka <am97veb@gmail.com>
2025-01-08 16:00:13 +0100

    API add

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import Form from "./Form";
 import Result from "./Form/Result";
 import Clock from "./Clock";
+import CurrencyInformation from "./CurrencyInformation";
 import { currencyTable } from "./currencyTable";
 import { useState } from "react";
 import { GlobalStyles } from "./globalStyle";
+// import currency from "./currency.json";
 
 function App() {
   const [currencyName, setCurrency] = useState("EUR");
@@ -20,6 +22,33 @@ function App() {
     });
   };
 
+
+
+
+  fetch("courrency_converter_react/currency.json")
+    .then(response => response.json())
+    .then(currency => console.log(currency))
+    .catch(error => console.error("Ups...coś poszło nie tak.", error));
+
+
+
+
+  // (async () => {
+  //   try{
+  //     const response = await fetch("currency.json");
+
+  //     if (!response.ok) {
+  //       throw new Error (response.statusText);
+  //     }
+  //     const currency = await response.json();
+  //     console.log(currency);
+  //   }
+  //   catch (error) {
+  //     console.error("ups...coś poszło źle", error);
+  //   }
+  // }) ();
+
+
   return (
 
     <main>
@@ -31,6 +60,7 @@ function App() {
         currencyName={currencyName}
         amountValue={amountValue}
         setAmount={setAmount}
+        currencyInformation={<CurrencyInformation />}
         result={<Result
           amountValue={amountValue}
           result={result}
