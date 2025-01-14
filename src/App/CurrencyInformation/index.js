09ebd072f4633cb3d66a5a44fcf6d543604d6c36
Adrian Markefka <am97veb@gmail.com>
2025-01-14 10:18:17 +0100

    SideInformation outside StyledInformation and change margin in both components

--- src/App/CurrencyInformation/index.js
+++ src/App/CurrencyInformation/index.js
@@ -1,25 +1,26 @@
 import { StyledInformation, SideInformation } from "./styled";
 
-const CurrencyInformation = ({ currencyData }) => {
+const CurrencyInformation = ({ dataState }) => {
 
-    const currencyDate = Object.values(currencyData.meta);
+    const currencyDate = Object.values(dataState.currencyData.meta);
     const newDate = new Date(currencyDate);
 
     return (
-        <StyledInformation>
-            Kursy walut pochodzą ze strony
-            {" "}
-            <a target="_blank" rel="noreferrer noopener" href="https://currencyapi.com/">currencyapi.com</a>*
-            {" "}
-            i&nbsp;są&nbsp;aktualne&nbsp;na&nbsp;dzień
-            {" "}
-            {newDate.toLocaleDateString(undefined, { month: "long", year: "numeric", day: "numeric" })}
-            &nbsp;roku.
-            <br></br>
+        <>
+            <StyledInformation>
+                Kursy walut pochodzą ze strony
+                {" "}
+                <a target="_blank" rel="noreferrer noopener" href="https://currencyapi.com/">currencyapi.com</a>*
+                {" "}
+                i&nbsp;są&nbsp;aktualne&nbsp;na&nbsp;dzień
+                {" "}
+                {newDate.toLocaleDateString(undefined, { month: "long", year: "numeric", day: "numeric" })}
+                &nbsp;roku.
+            </StyledInformation>
             <SideInformation>
                 *strona otwiera się w nowej karcie.
             </SideInformation>
-        </StyledInformation>
+        </>
     );
 };
 
--- src/App/CurrencyInformation/styled.js
+++ src/App/CurrencyInformation/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledInformation = styled.p`
-    margin-top: 30px;
+    margin: 30px 0 5px;
     font-weight: normal;
     font-size: 14px;
     text-align: center;
@@ -12,4 +12,5 @@ export const SideInformation = styled.p`
     font-weight: normal;
     font-size: 12px;
     text-align: left;
+    margin-bottom: 25px
 `;
