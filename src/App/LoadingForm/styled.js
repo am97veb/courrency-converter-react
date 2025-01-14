ca0417c33795f148f249376631d753cbe1d0a1a9
Adrian Markefka <am97veb@gmail.com>
2025-01-14 10:30:46 +0100

    unnecessary components removed

--- src/App/LoadingForm/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const LoadingText = styled.p`
-    text-align: center;
-    margin: 10%;
-`;
--- src/App/currencyTable.js
+++
@@ -1,5 +0,0 @@
-export const currencyTable = [
-    { id: 1, name: "EUR", value: 4.34 },
-    { id: 2, name: "USD", value: 4.03 },
-    { id: 3, name: "CHF", value: 4.57 },
-  ];
